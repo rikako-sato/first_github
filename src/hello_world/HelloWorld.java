dd95c8f15a9f90ef20f6e0ed09e6729140acc802
z2160040 <z2160040@192.168.128.107>
2017-07-26 16:04:21 +0900

    コミット2回目(hello 世界）に修正

--- src/hello_world/HelloWorld.java
+++ src/hello_world/HelloWorld.java
@@ -4,8 +4,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World");
-		
+		System.out.println("Hello 世界");
+
 
 	}
 
